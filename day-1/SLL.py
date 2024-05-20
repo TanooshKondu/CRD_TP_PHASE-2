fca8182fc25c67961a4667bf77ad797d994ed833
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-05-20 15:43:46 +0530

    final update day-1

--- day-1/SLL.py
+++ day-1/SLL.py
@@ -1,4 +1,5 @@
 # #10, 15, 20, 5, 18
+# https://pastebin.com/S1DqVuSn
 
 # class Box:
 #     def __init__(self, data):
@@ -68,3 +69,66 @@ for ele in nums:
 # obj3.next = obj4
 # obj4.next = None
 printLinkedList(head)
+
+
+
+
+###<!-------Sir Code--------!>
+
+class Box:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None
+ 
+def printLinkedList(curr):
+    while curr != None:
+        print(curr.data, end = " --> ")
+        curr = curr.next 
+    print()
+ 
+def insertAtTailNode(head, ele):
+    temp = Box(ele) 
+    if head == None:
+        return temp
+    tail = head 
+ 
+    while tail.next != None:
+        tail = tail.next 
+    tail.next = temp
+    return head 
+ 
+def insertAtBeginning(head, ele):
+    temp = Box(ele)
+    if head == None:
+        return temp 
+    temp.next = head 
+    return temp
+ 
+def insertAtSpecificPosition(head, position, ele):
+    if position == 0:
+        return insertAtBeginning(head, ele)
+ 
+    temp = Box(ele)
+    currentIndex = 0 
+    currentNode = head 
+    while currentIndex != position - 1:
+        currentIndex += 1 
+        currentNode = currentNode.next 
+
+    temp.next = currentNode.next 
+    currentNode.next = temp 
+    return head
+
+head = None
+nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+for ele in nums:
+    head = insertAtBeginning(head, ele)
+
+printLinkedList(head)
+head = insertAtSpecificPosition(head, 5, 1899)
+printLinkedList(head)
+
+# n = int(input())
+# for i in range(n):
+#     ele = int(input())
+#     head = insertAtTailNode(head, ele)
