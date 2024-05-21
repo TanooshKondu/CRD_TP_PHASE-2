5c3e386fe5086702d2ca1eefb332156c5fe02be0
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-05-21 14:08:21 +0530

    updated on day-2

---
+++ day-2/nextGreaterElement.py
@@ -0,0 +1,19 @@
+#finding next greater element
+
+def findNextGreaterElement(nums):
+    stack = []
+    n = len(nums)
+    result = [-1] * n
+    
+    for index in range(n - 1, -1, -1):
+        while len(stack) > 0 and stack[0] < nums[index]:
+            stack.pop(0)
+        if len(stack) > 0:
+            result[index] = stack[0]
+        stack.insert(0, nums[index])
+        
+    return result
+
+nums = [12, 10, 4, 15, 9, 200, 121, 154, 12]
+result = findNextGreaterElement(nums)
+print(result)
