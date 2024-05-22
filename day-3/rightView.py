1e5f36452bd1b8d66ce8aa3f70ea12a2c2e839b2
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-05-22 13:46:05 +0530

    updated on day-3

---
+++ day-3/rightView.py
@@ -0,0 +1,52 @@
+#find Right View
+class Box:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def findRightView(root):
+    if root == None:
+        return []
+    result = []
+    Q = [root]
+    while len(Q) > 0:
+        n = len(Q)
+        for i in range(n):
+            curr = Q.pop(0)
+            if i == (n-1):
+                result.append(curr.data)
+            if curr.left != None:
+                Q.append(curr.left)
+            if curr.right != None:
+                Q.append(curr.right)
+    return result
+
+obj1 = Box(10)
+obj2 = Box(20)
+obj3 = Box(30)
+obj4 = Box(40)
+obj5 = Box(50)
+obj6 = Box(60)
+obj7 = Box(70)
+obj8 = Box(90)
+obj9 = Box(100)
+obj10 = Box(-40)
+
+# establishing links among those nodes
+obj1.left = obj2
+obj1.right = obj3 
+obj2.right = obj4
+obj2.right = obj5 
+obj4.right = obj6 
+obj6.left = obj7 
+obj5.right = obj8 
+obj5.left = obj9 
+obj9.right = obj10 
+
+
+print(findRightView(obj1))
+#output
+#[10, 30, 50, 90, -40]
+
+#=== Code Execution Successful ===
