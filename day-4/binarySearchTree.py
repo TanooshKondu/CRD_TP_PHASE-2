eb3ca923dc2ad4694b12fd48d3b83512f9e2a58f
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-05-23 10:26:57 +0530

    updated on day-4

--- day-4/binarySearchTree.py
+++ day-4/binarySearchTree.py
@@ -1,5 +1,4 @@
 #Binary search Tree
-#https://pastebin.com/
 #leetcode 701
 # Definition for a binary tree node.
 # class TreeNode:
@@ -17,3 +16,31 @@
 #             root.right = self.insertIntoBST(root.right, val)
 #         return root
 
+#<<<<<<<<-----Delete BST------>>>>>>>>>>
+#leetcode 450
+#https://pastebin.com/tBYc8fJb
+
+def findInorderPredessor(root):
+    while root.left != None:
+        root = root.left 
+    return root.data
+
+def deleteInBST(root, val):
+    if root == None:
+        return None 
+    elif root.data > val:
+        root.left = deleteInBST(root.left, val)
+    elif root.data < val:
+        root.right = deleteInBST(root.right, val)
+    else:
+        if root.left == None and root.right == None:
+            return None 
+        elif root.left == None:
+            return root.right 
+        elif root.right == None:
+            return root.left 
+        else:
+            predessor = findInorderPredessor(root)
+            root.data = predessor 
+            root.right = deleteInBST(root.right, predessor)
+        return root
